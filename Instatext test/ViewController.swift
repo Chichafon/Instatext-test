a87dfeb2c59000a56c59a78171fb01c989f39d11
chichafon <chichafonchichafon@gmail.com>
2022-01-23 15:49:32 +0400

    Initial Commit

---
+++ Instatext test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instatext test
+//
+//  Created by 111 on 23.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
