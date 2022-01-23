1dfb58cf94119e6cdf459a7b4368df53eb4c106d
chichafon <chichafonchichafon@gmail.com>
2022-01-23 16:23:03 +0400

    Create MVP structure.
    create new folders, delete storybord, prepare for GIT flow.

---
+++ Instatext test/Sources/App/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Instatext test
+//
+//  Created by 111 on 23.01.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
