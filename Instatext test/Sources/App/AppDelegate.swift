24a1c3d264944a73d40131897a2008cbdb426c9b
chichafon <chichafonchichafon@gmail.com>
2022-01-25 00:46:55 +0400

    Create save button.
    add action on button.

--- Instatext test/Sources/App/AppDelegate.swift
+++ Instatext test/Sources/App/AppDelegate.swift
@@ -14,8 +14,9 @@ var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        let navigationController = UINavigationController.init(rootViewController: TextField())
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
 
         return true
--- Instatext test/Sources/Scenes/View/TextField.swift
+++ Instatext test/Sources/Scenes/View/TextField.swift
@@ -34,11 +34,47 @@ class TextField: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(nitification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Сохранить", style: .plain, target: self, action: #selector(buttonAction))
+//        checkTextField()
+
         view.addSubview(myTextFieldView)
         view.backgroundColor = .white
     }
 
     //MARK: - Actions
+
+    @objc func checkTextField() {
+        if myTextFieldView.text.isEmpty {
+            navigationItem.rightBarButtonItem?.isEnabled = false
+        } else {
+            navigationItem.rightBarButtonItem?.isEnabled = true
+        }
+    }
+
+    @objc func buttonAction() -> UIImage {
+
+        UIGraphicsBeginImageContextWithOptions(self.myTextFieldView.bounds.size, false, 0)
+        let context = UIGraphicsGetCurrentContext()
+        for photoObject : AnyObject in UIApplication.shared.windows {
+            if let window = photoObject as? UIWindow {
+                if window.responds(to: #selector(getter: UIWindow.screen)) || window.screen == UIScreen.main {
+                    context!.saveGState();
+                    context!.translateBy(x: window.center.x,
+                                         y: window.center.y);
+                    context!.concatenate(window.transform);
+                    context!.translateBy(x: -window.bounds.size.width * window.layer.anchorPoint.x,
+                                         y: -window.bounds.size.height * window.layer.anchorPoint.y);
+                    window.layer.render(in: context!)
+                    context!.restoreGState();
+                }
+            }
+        }
+
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return UIImage() }
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+
+        return image
+    }
 
     @objc func keyboardWillShow(notification: NSNotification) {
         guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
